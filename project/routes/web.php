4ec25e71a4c113f05e49d0cab89db9145cc301d1
mathanraj1219 <mathan1912raj@gmail.com>
2024-08-21 21:38:17 +0530

    commit

--- project/routes/web.php
+++ project/routes/web.php
@@ -2,16 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MoUController;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
 
 Route::get('/', function () {
     return view('dashboard');
@@ -22,4 +13,7 @@ Route::get('/departments',function() {
 });
 Route::get('/outcomes',function() {
     return view('outcomes');
+});
+Route::get('/MoUs',function() {
+    return view('mou');
 });
